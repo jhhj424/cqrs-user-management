82023ccf40ad8b978c9616c7155fe4ae0ca156d1
zito-s <jhhj424@naver.com>
2022-09-06 18:36:11 +0900

    Initial commit

---
+++ user-cmd-api/src/main/java/com/springbank/user/cmd/api/UserCmdApiApplication.java
@@ -0,0 +1,13 @@
+package com.springbank.user.cmd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCmdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCmdApiApplication.class, args);
+    }
+
+}
