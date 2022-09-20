41f7a6160a0744a1612ffc8a06900056c6ab9ec3
zito-s <zito.s@kakaocorp.com>
2022-09-20 17:25:15 +0900

    feat: 유저 등록 컨트롤러 구현

--- user-core/src/main/java/com/springbank/user/core/models/Account.java
+++ user-core/src/main/java/com/springbank/user/core/models/Account.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -13,7 +15,10 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class Account {
+    @Size(min = 2, message = "사용자 이름은 최소 2자 이상이어야 합니다.")
     private String username;
+    @Size(min = 7, message = "비밀번호는 최소 7자 이상이어야 합니다.")
     private String password;
+    @NotNull(message = "사용자 역할을 1개 이상 지정")
     private List<Role> roles;
 }
--- user-core/src/main/java/com/springbank/user/core/models/User.java
+++ user-core/src/main/java/com/springbank/user/core/models/User.java
@@ -7,6 +7,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,8 +19,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id;
+    @NotEmpty(message = "이름은 필수입니다")
     private String firstname;
+    @NotEmpty(message = "성은 필수입니다")
     private String lastname;
+    @Email(message = "유효한 이메일 주소를 입력하세요")
     private String emailAddress;
+    @NotNull(message = "계정 자격 증명을 입력하세요")
     private Account account;
 }
