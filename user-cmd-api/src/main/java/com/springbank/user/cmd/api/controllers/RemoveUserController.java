820cd897ec190a4639f7e67225abac46b2981ce0
zito-s <jhhj424@naver.com>
2022-09-21 11:23:06 +0900

    feat: 유저 삭제 api 구현

---
+++ user-cmd-api/src/main/java/com/springbank/user/cmd/api/controllers/RemoveUserController.java
@@ -0,0 +1,35 @@
+package com.springbank.user.cmd.api.controllers;
+
+import com.springbank.user.cmd.api.commands.RemoveUserCommand;
+import com.springbank.user.cmd.api.dto.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/api/v1/removeUser")
+public class RemoveUserController {
+    private final CommandGateway commandGateway;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BaseResponse> removeUser(@PathVariable(value = "id") String id) {
+        try {
+            commandGateway.send(new RemoveUserCommand(id));
+
+            return new ResponseEntity<>(new BaseResponse("User successfully removed!"), HttpStatus.OK);
+        } catch (Exception e) {
+            var safeErrorMessage = "Error while processing remove user request for id - " + id;
+            log.error(e.toString());
+
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
