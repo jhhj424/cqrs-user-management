bc9033bf5f66137d04dc15fceceb2f4fe42b33e9
zito-s <jhhj424@naver.com>
2022-09-21 10:48:24 +0900

    feat: 유저 업데이트 api 구현

--- user-cmd-api/src/main/java/com/springbank/user/cmd/api/commands/UpdateUserCommand.java
+++ user-cmd-api/src/main/java/com/springbank/user/cmd/api/commands/UpdateUserCommand.java
@@ -5,10 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 public class UpdateUserCommand {
     @TargetAggregateIdentifier
     private String id;
+    @NotNull(message = "사용자 세부 정보가 제공되지 않았습니다.")
+    @Valid
     private User user;
 }
---
+++ user-cmd-api/src/main/java/com/springbank/user/cmd/api/controllers/UpdateUserController.java
@@ -0,0 +1,36 @@
+package com.springbank.user.cmd.api.controllers;
+
+import com.springbank.user.cmd.api.commands.UpdateUserCommand;
+import com.springbank.user.cmd.api.dto.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/api/v1/updateUser")
+public class UpdateUserController {
+    private final CommandGateway commandGateway;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BaseResponse> updateUser(@PathVariable(value = "id") String id,
+                                                   @Valid @RequestBody UpdateUserCommand command) {
+        try {
+            command.setId(id);
+            commandGateway.send(command);
+
+            return new ResponseEntity<>(new BaseResponse("User successfully updated!"), HttpStatus.OK);
+        } catch (Exception e) {
+            var safeErrorMessage = "Error while processing update user request for id - " + id;
+            log.error(e.toString());
+
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
