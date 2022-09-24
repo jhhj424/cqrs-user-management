b6b0d1403a81f07fd195d34c2995a31ef2a713a8
zito-s <jhhj424@naver.com>
2022-09-24 17:25:12 +0900

    feat: 인증서버 유저 서비스 추가

---
+++ user-oauth-2/src/main/java/com/springbank/user/oauth2/services/UserService.java
@@ -0,0 +1,37 @@
+package com.springbank.user.oauth2.services;
+
+import com.springbank.user.oauth2.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("잘못된 사용자 이름 / 암호가 제공됨");
+        }
+
+        var account = user.get().getAccount();
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(username)
+                .password(account.getPassword())
+                .authorities(account.getRoles())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
